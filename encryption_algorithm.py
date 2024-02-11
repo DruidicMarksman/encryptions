70d7a3d8b7094c9a811024e70401ba16be2436fa
DruidicMarksman <156147977+DruidicMarksman@users.noreply.github.com>
2024-02-10 19:04:33 -0500

    Update encryption_algorithm.py

--- encryption_algorithm.py
+++ encryption_algorithm.py
@@ -25,12 +25,12 @@ def encrypt(string):
     # i got them from nerds on google
     msg_unencrypted = string # msg is for the encrypted version
     mybytes = msg_unencrypted.encode('utf-8') + b'\x01'  # Pad with 1 to preserve trailing zeroes
-    msg = int.from_bytes(mybytes, 'little')*math.pi
+    msg = int.from_bytes(mybytes, 'little')/math.pi
     encrypted = [msg,secret_number]
     return encrypted
 def decrypt(encrypted_message_as_list):
     """ Decrypts the ENCRYPTED_MESSAGE from effectively random numbers to actual text"""
-    encrypted_message = encrypted_message_as_list[0]/math.pi
+    encrypted_message = encrypted_message_as_list[0]*math.pi
     secret_number = encrypted_message_as_list[1]
     real_message = int(encrypted_message/secret_number)
     recoveredbytes = real_message.to_bytes((encrypted_message.bit_length() + 7) // 8, 'little')
